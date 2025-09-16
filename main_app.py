d76edd5aefa9f756739f28b23fa85d17ccdd8282
simone81roll <simone_spiritelli@tiscali.it>
2025-09-16 15:09:11 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,21 +1,28 @@
 import streamlit as st
 
+st.set_page_config(page_title="Python - Funzioni Utili", page_icon="🐍") 
+
+st.sidebar.image("python_logo.png", width=250) 
+
 pages = {
     "Pandas":[
-        st.Page("pandas_comand.py", title="Comandi Pandas", icon=":material/keyboard:"),
+        st.Page("pandas_comand.py", title="Comandi Pandas", icon=":material/data_table:"),
     ],
     "Numpy":[
-        st.Page("numpy_comand.py", title="Comandi NumPy", icon=":material/keyboard:"),
+        st.Page("numpy_comand.py", title="Comandi NumPy", icon=":material/calculate:"),
     ],    
     "Excel e CSV":[
-        st.Page("file_comand.py", title="Leggi/scrivi/salva/esporta", icon=":material/format_list_numbered:"),
+        st.Page("file_comand.py", title="Leggi/scrivi/salva/esporta", icon=":material/folder:"),
     ],
     "DataBase":[
-        st.Page("sqlite_comand.py", title="Principali comandi con SQLite3", icon=":material/analytics:"),
+        st.Page("sqlite_comand.py", title="Principali comandi con SQLite3", icon=":material/database:"),
     ],
+    "HTML":[
+        st.Page("jinja2_comand.py", title="Principali comandi con Jinja2", icon=":material/code:"),
+    ],    
     "Varie":[
-        st.Page("date_manipolation_comand.py", title="Operazioni sulle date", icon=":material/analytics:"),
-        st.Page("istruzioni.py", title="Info utili", icon=":material/analytics:"),
+        st.Page("date_manipolation_comand.py", title="Operazioni sulle date", icon=":material/today:"), # Ho corretto il typo qui
+        st.Page("istruzioni.py", title="Info utili", icon=":material/info:"),
     ],        
 }
 
