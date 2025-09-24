e2531b8ada284207367f15777fb77c3e4ea2463c
simone81roll <simone_spiritelli@tiscali.it>
2025-09-24 10:35:00 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -8,38 +8,31 @@ pages = {
     "Home":[
         st.Page("home.py", title="Home Page", icon=":material/home:"),
     ],
-    "Pandas":[
-        st.Page("pandas_comand.py", title="Comandi Pandas", icon=":material/data_table:"),
+    "Analisi Dati":[
+        st.Page("pandas_comand.py", title="Comandi Pandas", icon=":material/storage:"),
+        st.Page("numpy_comand.py", title="Comandi NumPy", icon=":material/superscript:"),
+        st.Page("grafici_comand.py", title="Realizzazione di grafici", icon=":material/pie_chart:"),
     ],
-    "Numpy":[
-        st.Page("numpy_comand.py", title="Comandi NumPy", icon=":material/calculate:"),
-    ],    
-    "Excel e CSV":[
-        st.Page("file_comand.py", title="Leggi/scrivi/salva/esporta", icon=":material/folder:"),
+    "Gestione File e I/O":[
+        st.Page("file_comand.py", title="Excel e CSV", icon=":material/folder_code:"),
     ],
-    "DataBase":[
+    "Database":[
         st.Page("sqlite_comand.py", title="Principali comandi con SQLite3", icon=":material/database:"),
     ],
-    "HTML":[
-        st.Page("jinja2_comand.py", title="Principali comandi con Jinja2", icon=":material/code:"),
-    ],   
-    "e-mail":[
-        st.Page("email_comand.py", title="Principali comandi per l'invio di mail", icon=":material/alternate_email:"),
+    "Sviluppo Web":[
+        st.Page("email_comand.py", title="Invio e-mail", icon=":material/alternate_email:"),
+        st.Page("jinja2_comand.py", title="HTML con Jinja2", icon=":material/code:"),    
     ],      
-    "Grafici":[
-        st.Page("grafici_comand.py", title="Realizzazione di grafici", icon=":material/pie_chart:"),
-    ],   
-    "Machine Learning":[
-        st.Page("machine_learning_comand.py", title="Esempi pratici di Machine Learning", icon=":material/batch_prediction:"),
-    ],            
-    "Varie":[
+    "Strumenti Utili":[
         st.Page("task_comand.py", title="Task", icon=":material/checklist_rtl:"),
-        st.Page("date_manipulation_comand.py", title="Operazioni sulle date", icon=":material/today:"),
+        st.Page("date_manipulation_comand.py", title="Manipolazione di date", icon=":material/today:"),
         st.Page("istruzioni.py", title="Info utili", icon=":material/info:"),
         st.Page("guida_deployment.py", title="Guida al deployment", icon=":material/deployed_code_update:"),
-    ],        
+    ],      
+    "Machine Learning":[
+        st.Page("machine_learning_comand.py", title="Esempi pratici", icon=":material/batch_prediction:"),
+    ],      
 }
 
 pg = st.navigation(pages)
 pg.run()
-
